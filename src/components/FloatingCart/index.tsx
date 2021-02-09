73583f6b99129507ce4d8560cc608a712787220a
Isaque Prestes Ferreira - DBServer <isaque.dbserver@superdigital.com.br>
2021-02-08 23:29:13 -0300

    resolve challenge rn

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,21 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((accumulate, product) => {
+      const totalPrice = product.price * product.quantity;
 
-    return formatValue(0);
+      return accumulate + totalPrice;
+    }, 0)
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    const total = products.reduce((accumulate, product) => {
+      const quantity = product.quantity;
+      return accumulate + quantity;
+    }, 0);
+    return total;
   }, [products]);
 
   return (
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -26,6 +26,7 @@ import {
 import { useCart } from '../../hooks/cart';
 
 import formatValue from '../../utils/formatValue';
+import { PriceContainer } from '../Dashboard/styles';
 
 interface Product {
   id: string;
@@ -39,23 +40,29 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id)
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulate, product) => {
+      const totalPrice = product.price * product.quantity;
 
-    return formatValue(0);
+      return accumulate + totalPrice;
+    }, 0)
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    const total = products.reduce((accumulate, product) => {
+      const quantity =  product.quantity;
+      return accumulate + quantity;
+    }, 0);
+    return total;
   }, [products]);
 
   return (
@@ -107,7 +114,7 @@ const Cart: React.FC = () => {
       </ProductContainer>
       <TotalProductsContainer>
         <FeatherIcon name="shopping-cart" color="#fff" size={24} />
-        <TotalProductsText>{`${totalItensInCart} itens`}</TotalProductsText>
+        <TotalProductsText>{totalItensInCart > 1 ? `${totalItensInCart} itens` : `${totalItensInCart} item` }</TotalProductsText>
         <SubtotalValue>{cartTotal}</SubtotalValue>
       </TotalProductsContainer>
     </Container>
